3446c45556c702522243d714b36991e537780d66
Leonardo <leonardoyaoki@gmail.com>
2021-10-30 21:57:08 -0300

    Atualização do Top-Bar e imgs adicionadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HomeComponent } from './components/main/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
