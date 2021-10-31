10844dd547e0aef472d3980a98c5004c0daf1103
Leonardo <leonardoyaoki@gmail.com>
2021-10-30 23:00:45 -0300

    Rotas, título e organização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/main/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
